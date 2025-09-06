79e28e5e382153ac411d587b003835e9b075b1bb
Dharaneeswar-Reddy-Avula <avuladharaniswarreddy@gmail.com>
2025-09-06 10:07:35 +0530

    Updated Ui version 3

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -122,7 +122,6 @@ const Contact = () => {
     <div className="min-h-screen u-display-s bg-[#f0eee6]">
       {/* Hero Section */}
       <section className="relative py-20 md:py-16">
-        <div className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')] bg-cover bg-center opacity-40"></div>
         <div className="container mx-auto px-4 relative">
           <motion.div 
             className="max-w-3xl mx-auto text-center"
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import {Link} from "react-router-dom"
 import { 
   CodeBracketIcon, 
   DevicePhoneMobileIcon, 
